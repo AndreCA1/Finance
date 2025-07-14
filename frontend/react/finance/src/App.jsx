6caef700a2fde1a7535e4fe54c0dde2628bf6e80
Ivan Marcelino <ivanmarcelino2004@gmail.com>
2025-07-14 00:26:34 -0300

    add mensagem de retorno pro usuario

--- frontend/react/finance/src/App.jsx
+++ frontend/react/finance/src/App.jsx
@@ -2,22 +2,37 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import PrivateRoute from "./utils/PrivateRoute";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route
-          path="/dashboard"
-          element={
-            //            <PrivateRoute>
-            <Dashboard />
-            //          </PrivateRoute>
-          }
-        />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route
+            path="/dashboard"
+            element={
+              //            <PrivateRoute>
+              <Dashboard />
+              //          </PrivateRoute>
+            }
+          />
+        </Routes>
+      </Router>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </>
   );
 }
 
